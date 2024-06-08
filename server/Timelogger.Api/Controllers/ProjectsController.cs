57029f4d560898b187b2f6f3361ace94a1afbb1a
Nathalia Lima <95.nathallya@gmail.com>
2024-06-08 15:31:53 +0100

    :sparkles: feat: updated projects get

--- server/Timelogger.Api/Controllers/ProjectsController.cs
+++ server/Timelogger.Api/Controllers/ProjectsController.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Timelogger.Enums;
 
 namespace Timelogger.Api.Controllers
 {
+	[Authorize]
 	[Route("api/[controller]")]
 	public class ProjectsController : Controller
 	{
@@ -16,14 +21,43 @@ namespace Timelogger.Api.Controllers
 		[Route("hello-world")]
 		public string HelloWorld()
 		{
-			return "Hello Back!";
+			var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "Id");
+			if (userIdClaim == null)
+			{
+				return "Unauthorized";
+			}
+
+			return userIdClaim.ToString();
 		}
 
 		// GET api/projects
 		[HttpGet]
 		public IActionResult Get()
 		{
-			return Ok(_context.Projects);
+			var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "Id");
+
+			if (userIdClaim == null)
+			{
+				return Unauthorized();
+			}
+
+			var userRoleClaim = User.Claims.FirstOrDefault(c => c.Type == "Role");
+
+			var id = int.Parse(userIdClaim.Value);
+
+			if (userRoleClaim.Value == Role.Freelancer.ToString())
+			{
+				var freelancerProjects = _context.Projects.Where(p => p.FreelancerId == id);
+				return Ok(freelancerProjects);
+			}
+			else if (userRoleClaim.Value == Role.Customer.ToString())
+			{
+				var customerProjects = _context.Projects.Where(p => p.CustomerId == id);
+				return Ok(customerProjects);
+			}
+
+
+			return Ok();
 		}
 	}
 }
