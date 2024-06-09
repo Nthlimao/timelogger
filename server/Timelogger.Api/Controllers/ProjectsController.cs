0d1915be63429ab634bb422f678e1cf7a1357ace
Nathalia Lima <95.nathallya@gmail.com>
2024-06-09 02:44:08 +0100

    :hammer: feat: added paged result to Projects

--- server/Timelogger.Api/Controllers/ProjectsController.cs
+++ server/Timelogger.Api/Controllers/ProjectsController.cs
@@ -1,8 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Timelogger.Dto;
 using Timelogger.Entities;
 using Timelogger.Enums;
 
@@ -20,7 +23,65 @@ namespace Timelogger.Api.Controllers
 		}
 
 		[HttpGet]
-		public IActionResult GetProjects()
+		public ActionResult<PagedResultDTO<ProjectDTO>> GetProjects(string sortBy = "Id",
+			string sortDirection = "asc",
+			int limit = 10, int page = 1)
+		{
+			var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "Id");
+
+			if (userIdClaim == null)
+			{
+				return Unauthorized();
+			}
+
+			var userRoleClaim = User.Claims.FirstOrDefault(c => c.Type == "Role");
+			var userId = int.Parse(userIdClaim.Value);
+
+			IQueryable<Project> queryProjects = null;
+
+			if (userRoleClaim.Value == Role.Freelancer.ToString())
+			{
+				queryProjects = _context.Projects.Where(p => p.FreelancerId == userId).OrderBy(p => p.Id);
+			}
+			else if (userRoleClaim.Value == Role.Customer.ToString())
+			{
+				queryProjects = _context.Projects.Where(p => p.CustomerId == userId);
+			}
+			else
+			{
+				return Ok();
+			}
+
+			queryProjects = SortProjects(queryProjects, sortBy, sortDirection);
+
+			limit = Math.Max(limit, 10);
+
+			var totalItems = queryProjects.Count();
+			var totalPages = (int)Math.Ceiling(totalItems / (double)limit);
+
+			page = Math.Max(page, 1);
+			page = page > totalPages ? totalPages : page;
+
+			var current = (page - 1) * limit;
+			var remainingItens = totalItems - current;
+			limit = remainingItens < limit ? remainingItens : limit;
+
+			queryProjects = queryProjects.Skip(current).Take(limit);
+
+			var projects = queryProjects.ToList();
+
+			return Ok(new PagedResultDTO<ProjectDTO>
+			{
+				Items = UpdateProjectsWithTimeSpent(projects),
+				PageNumber = page,
+				PageSize = limit,
+				TotalPages = totalPages,
+				TotalItems = totalItems
+			});
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<ProjectDTO> GetProject(int id)
 		{
 			var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "Id");
 
@@ -31,24 +92,18 @@ namespace Timelogger.Api.Controllers
 
 			var userRoleClaim = User.Claims.FirstOrDefault(c => c.Type == "Role");
 
-			var id = int.Parse(userIdClaim.Value);
+			var project = _context.Projects.Find(id);
+			var tasks = _context.Tasks.Where(t => t.ProjectId == project.Id).ToList();
 
-			if (userRoleClaim.Value == Role.Freelancer.ToString())
+			return Ok(new ProjectDTO(project)
 			{
-				var freelancerProjects = _context.Projects.Where(p => p.FreelancerId == id);
-				return Ok(freelancerProjects);
-			}
-			else if (userRoleClaim.Value == Role.Customer.ToString())
-			{
-				var customerProjects = _context.Projects.Where(p => p.CustomerId == id);
-				return Ok(customerProjects);
-			}
-
-			return Ok();
+				Tasks = tasks,
+				TotalTimeSpent = GetTotalTimeSpent(project)
+			});
 		}
 
 		[HttpPost]
-		public IActionResult CreateProject(Project project)
+		public ActionResult<Project> CreateProject(Project project)
 		{
 			var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "Id");
 
@@ -67,7 +122,7 @@ namespace Timelogger.Api.Controllers
 		}
 
 		[HttpPut("{id}")]
-		public IActionResult UpdateProject(int id, Project updatedProject)
+		public ActionResult<Project> UpdateProject(int id, Project updatedProject)
 		{
 			var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "Id");
 			if (userIdClaim == null)
@@ -85,6 +140,7 @@ namespace Timelogger.Api.Controllers
 
 			project.Name = updatedProject.Name;
 			project.CustomerId = updatedProject.CustomerId;
+			project.Deadline = updatedProject.Deadline;
 
 			_context.Entry(project).State = EntityState.Modified;
 			_context.SaveChanges();
@@ -122,5 +178,67 @@ namespace Timelogger.Api.Controllers
 			return NoContent();
 		}
 
+		private List<ProjectDTO> UpdateProjectsWithTimeSpent(List<Project> projects)
+		{
+			if (projects == null || projects.Count == 0)
+			{
+				return [];
+			}
+
+			var projectDTOs = new List<ProjectDTO>();
+
+			foreach (Project project in projects)
+			{
+				var projectDTO = new ProjectDTO(project)
+				{
+					TotalTimeSpent = GetTotalTimeSpent(project)
+				};
+
+				projectDTOs.Add(projectDTO);
+
+			}
+
+			return projectDTOs;
+		}
+
+		private int GetTotalTimeSpent(Project project)
+		{
+
+			if (project == null)
+			{
+				return 0;
+			};
+
+			var sumTimeSpent = _context.Tasks
+				.Where(t => t.ProjectId == project.Id)
+				.Sum(t => t.TimeSpent);
+
+			return sumTimeSpent;
+		}
+
+		private IQueryable<Project> SortProjects(IQueryable<Project> query, string sortBy, string sortDirection)
+		{
+			bool byDescending = sortDirection.ToLower() == "desc";
+
+			switch (sortBy.ToLower())
+			{
+				case "id":
+					query = byDescending ? query.OrderByDescending(p => p.Id) : query.OrderBy(p => p.Id);
+					break;
+
+				case "name":
+					query = byDescending ? query.OrderByDescending(p => p.Name) : query.OrderBy(p => p.Name);
+					break;
+
+				case "deadline":
+					query = byDescending ? query.OrderBy(p => p.Deadline) : query.OrderByDescending(p => p.Deadline);
+					break;
+
+				default:
+					break;
+			}
+
+			return query;
+		}
 	}
 }
